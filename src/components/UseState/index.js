fe55211a842aebd7b0e470a021d63e627af40c62
Daniel Legendre <122412456+Dlegendre1@users.noreply.github.com>
2023-10-14 17:31:33 +0000

    Update 4 files

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,18 +1,28 @@
-import './UseState.css';
+import { useState } from "react";
+import "./UseState.css";
 
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+
+  const setLight = () => {
+    setTheme("light");
+  };
+
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
   return (
-    <div className="state">
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
-      <button>Dark</button>
-      <button>Light</button>
-      <h2>DISPLAY COUNT HERE</h2>
-      <button>
-        Increment
-      </button>
-      <button>
-        Decrement
-      </button>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={setLight}>Light</button>
+      <h2>
+        COUNTER: {count} {count < 0 ? "NEGATIVE" : "POSITIVE"}
+      </h2>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 };
